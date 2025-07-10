f079431078c2532f60e6509f1fe58b50540dac06
Sukeerthi <sukeerthi.langu@gmail.com>
2025-07-10 19:08:20 +0530

    specified the tittle of the project used markdowm function under streamlit

--- buttons.py
+++ buttons.py
@@ -1,5 +1,14 @@
 import streamlit as st
 
+# for centre placement of title 
+# instead of using title we use markdowm which has an access to html code too
+st.markdown(
+    """
+    <h1 style='text-align: center;'>Talent Align AI</h1>
+    """,
+    unsafe_allow_html=True
+)
+
 #creating a resume button and 
 resume_state = st.button("Resume")
 jd_state = st.button("Job description")
