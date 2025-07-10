171f88437f205cb864e96f2cee1cab929d53ccd5
Sukeerthi <sukeerthi.langu@gmail.com>
2025-07-10 19:03:25 +0530

    Explored streamlit documentation and created 2 basic buttons that displayed on screen for uploading resume and job description

---
+++ buttons.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+#creating a resume button and 
+resume_state = st.button("Resume")
+jd_state = st.button("Job description")
+st.write(resume_state)
+st.write(jd_state)
+ 
+
+
